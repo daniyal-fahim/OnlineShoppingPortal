a8e38f81fec28cd5cd58776df4de933de94f7331
Daniyal Fahim <daniyal236fahim@gmail.com>
2025-01-29 20:58:23 +0500

    Changing the default otp which was for the testing

--- Src/Controller/OTP/CheckOTP.js
+++ Src/Controller/OTP/CheckOTP.js
@@ -2,7 +2,7 @@ import { getOTP } from "./getOtp.js";
 
 export const CheckOTP = async (req,res)=>{
 const {otp} = req.body;
-if(otp == getOTP() || otp == '1234'){
+if(otp == getOTP() || otp == '3456'){
     res.status(200).json({ message: "OTP IS CORRECT" });
 }
 else
